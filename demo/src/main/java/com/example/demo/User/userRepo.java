976d572fe1611d2742e89da5d3296eb0c1911290
©Ravindu_Sadhalkith <ravindusandhakith19@gmail.com>
2026-09-09 19:34:29 +0530

    Delete demo directory

--- demo/src/main/java/com/example/demo/User/userRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface userRepo extends JpaRepository<user,Long> {
-    Optional<user> findByUsername(String username);
-    Optional<user> findByphoneNumber(String phoneNumber);
-}
